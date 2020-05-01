26442ab608cbd3c4b5c3bf011f0383d1501f1605
Zzh_0313 <863907756@qq.com>
2020-05-01 10:08:51 -0400

    modified demomem-3

--- demomem-3/demomem.c
+++ demomem-3/demomem.c
@@ -17,11 +17,12 @@ struct demo_device{
     struct device *device;
 };
 
-static struct demo_device demo_dev;
+struct demo_device *demo_dev;
 
 static int demo_open(struct inode *node, struct file *file)
 {
     printk(KERN_INFO "Enter: %s\n",__func__);
+    file->private_data = (void *)demo_dev;
     return 0;
 }	
 
@@ -35,7 +36,8 @@ static ssize_t demo_read(struct file *file, char __user *buf, size_t size, loff_
 {
     int ret;
     int read_bytes;
-    char *kbuf = demo_dev.buffer + *pos;
+    struct demo_device *demo = file->private_data;
+    char *kbuf = demo->buffer + *pos;
 
     printk(KERN_INFO "Enter: %s\n",__func__);
     if(*pos >= BUF_SIZE) 
@@ -58,7 +60,8 @@ static ssize_t demo_write(struct file *file, const char __user *buf, size_t size
 {
     int ret;
     int write_bytes;
-    char *kbuf = demo_dev.buffer + *pos;
+    struct demo_device *demo = file->private_data;
+    char *kbuf = demo->buffer + *pos;
 
     printk(KERN_INFO "Enter: %s\n",__func__);
     if(*pos >= BUF_SIZE) 
@@ -89,18 +92,25 @@ static int __init demo_init(void)
     int ret = -1;
     dev_t dev_no;
 
-    demo_dev.buffer = (char *)kmalloc(BUF_SIZE,GFP_KERNEL);
-    if(!demo_dev.buffer) {
+    demo_dev = (struct demo_device *)kmalloc(sizeof(struct demo_device), GFP_KERNEL);
+    if(!demo_dev) {
+        printk(KERN_ERR "failed to malloc demo_device\n");
+        ret = -ENOMEM;
+        goto ERROR_MALLOC_DEVICE;
+    }
+
+    demo_dev->buffer = (char *)kmalloc(BUF_SIZE,GFP_KERNEL);
+    if(!demo_dev->buffer) {
         printk(KERN_ERR "malloc %d bytes failed\n", BUF_SIZE);
         ret = -ENOMEM;
-        goto ERROR_MALLOC;
+        goto ERROR_MALLOC_BUFFER;
     }
-    memset(demo_dev.buffer, 0x00, BUF_SIZE);
+    memset(demo_dev->buffer, 0x00, BUF_SIZE);
 
     dev_no = MKDEV(MAJOR_NUM,MINOR_NUM);
     printk(KERN_INFO "Enter: %s\n",__func__);
     //init for demo char
-    cdev_init(&demo_dev.cdev,&demo_opration);
+    cdev_init(&demo_dev->cdev,&demo_opration);
     //register device number
     ret = register_chrdev_region(dev_no,1,"demomem");
     if(ret<0) {
@@ -110,46 +120,52 @@ static int __init demo_init(void)
             goto ERROR_CHRDEV_REGION;
         }
     }
-    ret = cdev_add(&demo_dev.cdev,dev_no,1);
+    ret = cdev_add(&demo_dev->cdev,dev_no,1);
     if(ret<0) {
         printk(KERN_ERR "cdev add failed\n");
         goto ERROR_CDEV_ADD;
     } 
     //create a demomem class in /sys/class/demo/
-    demo_dev.cls = class_create(THIS_MODULE,"demo");
-    if(IS_ERR(demo_dev.cls)) {
-        ret = PTR_ERR(demo_dev.cls);
+    demo_dev->cls = class_create(THIS_MODULE,"demo");
+    if(IS_ERR(demo_dev->cls)) {
+        ret = PTR_ERR(demo_dev->cls);
         goto ERROR_CLASS_CEATE;
     } 
     //create a demomem device in /sys/class/demo/demomem
-    demo_dev.device = device_create(demo_dev.cls,NULL,dev_no,NULL,"demomem");
-    if(IS_ERR(demo_dev.device)) {
-        ret = PTR_ERR(demo_dev.device);
+    demo_dev->device = device_create(demo_dev->cls,NULL,dev_no,NULL,"demomem");
+    if(IS_ERR(demo_dev->device)) {
+        ret = PTR_ERR(demo_dev->device);
         goto ERROR_DEVICE_CREATE;
     }
     printk(KERN_INFO "demo init done\n");
    
     return 0;
 ERROR_DEVICE_CREATE:
-    class_destroy(demo_dev.cls);
+    class_destroy(demo_dev->cls);
 ERROR_CLASS_CEATE:
-    cdev_del(&demo_dev.cdev);
+    cdev_del(&demo_dev->cdev);
 ERROR_CDEV_ADD:
     unregister_chrdev_region(dev_no,1);
 ERROR_CHRDEV_REGION:
-    kfree(demo_dev.buffer);
-    demo_dev.buffer = NULL;
-ERROR_MALLOC:
+    kfree(demo_dev->buffer);
+    demo_dev->buffer = NULL;
+ERROR_MALLOC_BUFFER:
+    kfree(demo_dev);
+    demo_dev = NULL;
+ERROR_MALLOC_DEVICE:
     return ret;
 }
 
 static void __exit demo_exit(void)
 {
     printk(KERN_INFO "Enter: %s\n",__func__);
-    cdev_del(&demo_dev.cdev);
+    cdev_del(&demo_dev->cdev);
     unregister_chrdev_region(MKDEV(MAJOR_NUM,MINOR_NUM),1);
-    kfree(demo_dev.buffer);
-    demo_dev.buffer = NULL;
+    
+    kfree(demo_dev);
+    demo_dev = NULL;
+    kfree(demo_dev->buffer);
+    demo_dev->buffer = NULL;
     printk(KERN_INFO "demo exit done\n");
 }
 
