f9deaec25f44f609ed56ab2fed5068662de40c05
Zzh_0313 <863907756@qq.com>
2020-05-02 05:33:31 -0400

    modified demomem-4/demomem.c with misc device driver

--- demomem-4/demomem.c
+++ demomem-4/demomem.c
@@ -5,6 +5,7 @@
 #include <linux/device.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/miscdevice.h>
 #include "demomem.h"
 
 #define BUF_SIZE   1024
@@ -14,9 +15,7 @@
 struct demo_device{
     char *buffer;
     int value;
-    struct cdev cdev;
-    struct class *cls;
-    struct device *device;
+    struct miscdevice *mdev;
 };
 
 struct demo_device *demo_dev;
@@ -107,12 +106,18 @@ static long demo_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     return (long)ret;
 }
 
-static struct file_operations demo_opration = {
+static struct file_operations demo_operation = {
     .open = demo_open,
     .release = demo_release,
     .read = demo_read,
     .write = demo_write,
     .unlocked_ioctl = demo_ioctl,
+};
+
+static struct miscdevice misc_struct = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "demomem",
+    .fops = &demo_operation,
 };
 
 static int __init demo_init(void)
@@ -136,46 +141,16 @@ static int __init demo_init(void)
     memset(demo_dev->buffer, 0x00, BUF_SIZE);
     demo_dev->value = 1;
 
-    dev_no = MKDEV(MAJOR_NUM,MINOR_NUM);
-    printk(KERN_INFO "Enter: %s\n",__func__);
-    //init for demo char
-    cdev_init(&demo_dev->cdev,&demo_opration);
-    //register device number
-    ret = register_chrdev_region(dev_no,1,"demomem");
-    if(ret<0) {
-        ret = alloc_chrdev_region(&dev_no,0,1,"demomem");
-        if(ret < 0) { 
-            printk(KERN_ERR "failed to register device number\n");
-            goto ERROR_CHRDEV_REGION;
-        }
-    }
-    ret = cdev_add(&demo_dev->cdev,dev_no,1);
-    if(ret<0) {
-        printk(KERN_ERR "cdev add failed\n");
-        goto ERROR_CDEV_ADD;
-    } 
-    //create a demomem class in /sys/class/demo/
-    demo_dev->cls = class_create(THIS_MODULE,"demo");
-    if(IS_ERR(demo_dev->cls)) {
-        ret = PTR_ERR(demo_dev->cls);
-        goto ERROR_CLASS_CEATE;
-    } 
-    //create a demomem device in /sys/class/demo/demomem
-    demo_dev->device = device_create(demo_dev->cls,NULL,dev_no,NULL,"demomem");
-    if(IS_ERR(demo_dev->device)) {
-        ret = PTR_ERR(demo_dev->device);
-        goto ERROR_DEVICE_CREATE;
+    demo_dev->mdev = &misc_struct;
+    ret = misc_register(demo_dev->mdev);
+    if(ret < 0) {
+        printk(KERN_ERR "failed to register misc\n");
+        goto ERROR_MISC;
     }
     printk(KERN_INFO "demo init done\n");
    
     return 0;
-ERROR_DEVICE_CREATE:
-    class_destroy(demo_dev->cls);
-ERROR_CLASS_CEATE:
-    cdev_del(&demo_dev->cdev);
-ERROR_CDEV_ADD:
-    unregister_chrdev_region(dev_no,1);
-ERROR_CHRDEV_REGION:
+ERROR_MISC:
     kfree(demo_dev->buffer);
     demo_dev->buffer = NULL;
 ERROR_MALLOC_BUFFER:
@@ -188,9 +163,10 @@ ERROR_MALLOC_DEVICE:
 static void __exit demo_exit(void)
 {
     printk(KERN_INFO "Enter: %s\n",__func__);
-    cdev_del(&demo_dev->cdev);
-    unregister_chrdev_region(MKDEV(MAJOR_NUM,MINOR_NUM),1);
-    
+    //device_destroy(demo_dev->cls, MKDEV(MAJOR_NUM, MINOR_NUM));
+    //class_destroy(demo_dev->cls);//the 2 line is not add before
+    misc_deregister(demo_dev->mdev);
+
     kfree(demo_dev);
     demo_dev = NULL;
     kfree(demo_dev->buffer);
